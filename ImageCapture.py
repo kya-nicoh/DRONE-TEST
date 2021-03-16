e5b016c7ca5bb6d8e247487d86579eeee2e53ac8
GabbNicoh <gabbnicoh@gmail.com>
2021-03-16 15:34:59 +0800

    initialize

---
+++ ImageCapture.py
@@ -0,0 +1,14 @@
+import djitellopy as tello
+import cv2
+
+me = tello.Tello()
+me.connect()
+print(me.get_battery())
+
+me.streamon()
+
+while True:
+    img = me.get_frame_read().frame
+    # img = cv2.resize(img, (360, 240))
+    cv2.imshow("Drone Footage", img)
+    cv2.waitKey(1)
